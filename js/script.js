f4b07c49a0a5bfece82de8dd48a6f817e4202f25
allzyx <allzyx@proton.me>
2023-09-01 00:25:41 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function playSoundRight(){
+    var music = new Audio('music/right.mp3');
+    music.play();
+}
+
+function playSoundWrong(){
+    var music = new Audio('music/wrong.mp3');
+    music.play();
+}
